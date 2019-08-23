eea45d01fec838030f4e7dc11ac3dcad6bcc2e1c
gisellehargrove <gisellehargrove@gmail.com>
2019-08-23 12:39:01 -0400

    fixed redirect to go to correct url

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -11,7 +11,7 @@ const PrivateRoute = ({component: Component, ...rest}) => {
         localStorage.getItem('userToken') ? (
           <Component {...props} />
         ) : (
-          <Redirect to='/login' />
+          <Redirect to='/' />
         )
 
       } />
