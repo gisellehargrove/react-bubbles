903f7000213483bd6377fd0cbeaefe247ef120d5
gisellehargrove <gisellehargrove@gmail.com>
2019-08-23 11:18:45 -0400

    created login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
+
 
 const Login = () => {
+  const [creds, setCreds] = useState({
+    username: '',
+    password: ''
+  });
+
+
+  const handleChange = e => {
+    setCreds({
+      ...creds,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+
+  };
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div className='form-container'>
+      <form onSubmit={e => handleSubmit(e)}>
+        <input onChange={e => handleChange(e)} placeholder='username' value={creds.username} />
+        <input onChange={e => handleChange(e)} placeholder='password' value={creds.password} />
+        <button>Login</button>
+      </form>
+
+    </div>
   );
 };
 
---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+
+const PrivateRoute = () => {
+
+  return (
+    <div>Private Route</div>
+  );
+
+};
